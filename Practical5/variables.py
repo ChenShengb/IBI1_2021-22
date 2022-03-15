f58d7452c7cf37f1c6deaa21ec488d4551f63bcd
chenShengbo <shengbo.21@intl.zju.edu.cn>
2022-03-15 14:40:04 +0800

    edit variables.py

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -4,6 +4,13 @@ c=271
 d=b-c
 e=a-b
 print(d,e)
+if e>=d:
+ print("e is bigger")
+else:
+ print("d is bigger")
+List = [d,e]
+Max = max(List)
+print("the max is:", Max)
 x=1
 y=2
 w=bool("x" and "y")
