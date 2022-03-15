9c85165b7251dd430fa448800a81f21f4a294d7d
chenShengbo <shengbo.21@intl.zju.edu.cn>
2022-03-15 22:21:51 +0800

    variables.py

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -3,16 +3,16 @@ b=4218520
 c=271
 d=b-c
 e=a-b
-print(d,e)
+print(d,e) #show new COVID-19 caese 
 if e>=d:
- print("e is bigger")
+ print("e is bigger")   #print the greatest number of new COVID-19 cases
 else:
  print("d is bigger")
 List = [d,e]
 Max = max(List)
 print("the max is:", Max)
-x=1
+x=1 #set variables
 y=2
 w=bool("x" and "y")
-print(w)
+print(w) 
  
