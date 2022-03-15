6c02b41ab859ce1349380bb6b833645ea72620c3
chenShengbo <shengbo.21@intl.zju.edu.cn>
2022-03-15 22:24:44 +0800

    triangle.py

--- Practical5/triangle.py
+++ Practical5/triangle.py
@@ -1,4 +1,4 @@
-x=0
-for i in range(1,11):
-    x+=i
-    print(x)
+x=0 #set a variable
+for i in range(1,11):  #set the first 10 values of the triangle sequence
+    x+=i        #sum numbers 
+    print(x)    #get each result
