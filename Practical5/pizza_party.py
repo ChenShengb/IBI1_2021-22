33ce239fd79c47231c6067104420badf03c67164
chenShengbo <shengbo.21@intl.zju.edu.cn>
2022-03-16 10:17:59 +0800

    pizza_party.py

---
+++ Practical5/pizza_party.py
@@ -0,0 +1,10 @@
+n=0
+active = True  #set a judje variable
+while active:  #set while-loop to increase until p>=64 
+    p = (n * n + n + 2) / 2 #set the equation of p
+    if p<64:     
+        n = n+1 
+        print(n,p) #print slices for each number of straight cuts
+    else:
+        print("the enough pieces is", n+1) #print final correct answer
+        active = False
