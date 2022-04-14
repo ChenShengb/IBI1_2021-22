4580c61714796fe885d42a06848dbd5653dfb24b
chenShengbo <shengbo.21@intl.zju.edu.cn>
2022-04-14 21:21:00 +0800

    fragment_counter.py

--- Practical8/fragment_counter.py
+++ Practical8/fragment_counter.py
@@ -1,3 +1,3 @@
 seq = 'ATGCAATCGACTACGATCAATCGAGGGCC'
-a=seq.split('GA') # separate the sequence by indentify GA
+a=seq.split('GATTC') # separate the sequence by indentify GA
 print ('the total number of fragments is', len(a)) # print the total number
