b2824ebd343cc87e616b397548d563ec50289c0a
chenShengbo <shengbo.21@intl.zju.edu.cn>
2022-04-15 15:30:11 +0800

    count_cut_genes.py

---
+++ Practical8/count_cut_genes.py
@@ -0,0 +1,36 @@
+with open('/Users/yefen/IBI1_2021-22/Practical8/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa') as a:
+    fout=open('/Users/yefen/IBI1_2021-22/Practical8/count_cut_genes.fa','w') #output the results in a new fasta file
+    # I got help from mt classmate Xu Ziyi to indentify genes
+    EcoRI = 'GAATTC'
+    order = 0
+    seqs = []
+    gene_name = []
+    for line in a:        #to indentify each gene name
+        if line.startswith('>'):
+            gene_name.append(line)   #add gene name to the list
+            order = order + 1
+        if (line.startswith('>')) and (order != 1):   #order started from 0,
+            gene = ''
+            for seq in seqs:
+                gene = gene + str(seq) #add sequence and delete return
+            seqs = [] #remove the content in the seqs for the next cycle
+            i = 0
+            count=0
+            judge = True   #to indentify sequence with "GAATTC"
+            while (i <= len(gene)):
+                i = i + 1
+                if gene[i:i + 6] == EcoRI:
+                   count=count+1 #count the number of fragments
+            if count!=0:
+                name = gene_name[order-2]
+                a=str(name[0:8])+'     '  #all gene name have 7 characters
+                b=str(count)+'\n'
+                c=(gene)+'\n'
+                line1=a
+                line2=b
+                line3=c
+                fout.write(line1)     #write the file
+                fout.write(line2)
+                fout.write(line3)
+        if not line.startswith('>'):
+            seqs.append(line.replace('\n', '')) #to delete return back
