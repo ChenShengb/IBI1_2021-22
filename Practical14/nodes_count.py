3f5435d8f6482eadad3972daf8fa1b9d6a7cb858
chenShengbo <shengbo.21@intl.zju.edu.cn>
2022-05-23 09:30:53 +0800

    nodes_count.py

--- Practical14/nodes_count.py
+++ Practical14/nodes_count.py
@@ -1,60 +1,60 @@
-from xml.dom.minidom import parse
-import xml.dom.minidom
-import numpy as np
 import matplotlib.pyplot as plt
+import xml.dom.minidom
+from numpy import*
 
-# read and parse the go_obo.xml using DOM that I learned in the Monday IBI CLass
-DOMTree =xml.dom.minidom.parse("/Users/yefen/IBI1_2021-22/Practical14/go_obo.xml")
-collection = DOMTree.documentElement
-terms= collection.getElementsByTagName("term")
-print('the total number of terms:', len(terms)) # to show the total number of terms
+def find_parents(node_id, parent_ids):
+    immediate_parent_ids = node_map[node_id]
+    if len(immediate_parent_ids):
+        for immediate_parent_id in immediate_parent_ids:
+            parent_ids.add(immediate_parent_id)
+            find_parents(immediate_parent_id, parent_ids)
 
-# define a function that count the childnotes of terms and subtract a subset of duplicates
-def sum_nodes(id):       # Consistent internal and external variables
-    global childNodes_term, childNodes_term_nore
-    for j in range(len(is_a_all)):
-        for k in range(len(is_a_all[j])):
-            if is_a_all[j][k] == id:   #this means term has a childnote
-                childNodes_term.append(id_all[j])
-                sum_nodes(id_all[j])
-    for l in childNodes_term:
-        if l not in childNodes_term_nore:
-            childNodes_term_nore.append(l)
-id_all=[]
-is_a_all=[]
-for i in range(len(terms)):
-    is_a= terms[i].getElementsByTagName("is_a") #to find the is_a line
-    id_all.append(terms[i].getElementsByTagName("id")[0].childNodes[0].data)#to find the id line
-    is_a_all.append([])
-    # add the is_a in a term
-    for j in range(len(is_a)):
-        is_a_all[i].append(terms[i].getElementsByTagName("is_a")[j].childNodes[0].data)
-term_childNodes_all=[]
-translation=[]
-for i in range(len(id_all)):
-        childNodes_term=[]
-        childNodes_term_nore=[]
-        # count the childnotes
-        sum_nodes(id_all[i])
-        term_childNodes_all.append(len(childNodes_term_nore))
-#to get the translation related terms
-        if terms[i].getElementsByTagName("defstr")[0].childNodes[0].data.find("translation") != -1:
-            translation.append(len(childNodes_term_nore))
-# draw the boxplot of all terms and terms related to translation
-plt.boxplot(term_childNodes_all)
-plt.title=('distribution of child nodes across terms')
-plt.xlabel=("all terms in GO")
-plt.ylabel=('number')
+nodes = xml.dom.minidom.parse("/Users/yefen/IBI1_2021-22/Practical14/go_obo.xml").documentElement.getElementsByTagName("term")
+print("There are", len(nodes), "terms in go_obo.xml")
+node_map = {}
+result = {}
+resu = []
+judge=[]
+pic=[]
+for node in nodes:
+    node_id = node.getElementsByTagName("id")[0].childNodes[0].data
+    immediate_parent_ids = []
+    for is_a in node.getElementsByTagName("is_a"):
+        immediate_parent_ids.append(is_a.childNodes[0].data)
+    node_map[node_id] = immediate_parent_ids
+    if node.getElementsByTagName("defstr")[0].childNodes[0].data.find("translation") != -1:
+        judge.append(1)
+    else:
+        judge.append(0)
+    result[node_id] = 0
+
+for key in node_map.keys():
+
+    parent_ids = set()
+    find_parents(key, parent_ids)
+    for parent_id in parent_ids:
+        result[parent_id] += 1
+
+for i, (k, v) in enumerate(result.items()):
+    resu.append(v)
+    if judge[i]==1:
+        pic.append(v)
+
+plt.boxplot(result.values(), vert=True, whis=1.5, patch_artist=True, showbox=True, showcaps=True, showfliers=True)
+plt.title('Distribution of child node number of all GO terms')
+plt.xlabel("all GO terms")
+plt.ylabel("Number")
 plt.show()
-plt.boxplot(translation)
-plt.title=('distribution of child nodes across terms associated with translation')
-plt.xlabel=('related to translation')
-plt .ylabel=('number')
+plt.boxplot(pic, vert=True, whis=1.5, patch_artist=True, showbox=True, showcaps=True, showfliers=True)
+plt.title('Distribution of child node number of terms associated with ‘translation’')
+plt.xlabel("associated with ‘translation’")
+plt.ylabel("Number")
 plt.show()
-#compare the average
-a= sum(term_childNodes_all )/len(term_childNodes_all)
-b=sum(translation)/len(translation)
-if a > b:
-    print('The translation terms contain, on average, a lower number of childnodes than the overall Gene Ontology.')
+
+if mean(pic)<mean(resu):
+	print("the translation terms contain, on average, a smaller number of child nodes than the overall Gene Ontology")
+elif mean(pic)>mean(resu):
+	print("the translation terms contain, on average, a greater number of child nodes than the overall Gene Ontology")
 else:
-    print('The translation terms contain, on average, a greater number of childnodes than the overall Gene Ontology.')
+	print ("They contain an equal number of average child nodes")
+#comment: The "translation" terms contain, on average, a greater number of child nodes than the overall Gene Ontology
