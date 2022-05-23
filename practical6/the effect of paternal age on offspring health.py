0e086a78862d5af3225a61e666ea171c953a8b21
chenShengbo <shengbo.21@intl.zju.edu.cn>
2022-05-23 12:40:10 +0800

    the\ effect\ of\ paternal\ age\ on\ offspring\ health.py

--- practical6/the effect of paternal age on offspring health.py
+++ practical6/the effect of paternal age on offspring health.py
@@ -2,6 +2,8 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import numpy as np
+paternal_age=[30,35,40,45,50,55,60,65,70,75]
+chd=[1.03,1.07,1.11,1.17,1.23,1.32,1.42,1.55,1.72,1.94]
 x=np.array([30,35,40,45,50,55,60,65,70,75]) # set the variable of the requested paternal age
 y=np.array([1.03,1.07,1.11,1.17,1.23,1.32,1.42,1.55,1.72,1.94]) # set the variable of risk for congenital heart disease
 fig = plt.figure()
